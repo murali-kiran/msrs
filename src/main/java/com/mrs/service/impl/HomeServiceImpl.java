148d231695b341143927eb01fca6f3d1c7e270f0
Kiran Kumar <pusa.kirankumar@gmail.com>
2017-02-02 09:14:58 +0530

    Added logback for logging

--- src/main/java/com/mrs/service/impl/HomeServiceImpl.java
+++ src/main/java/com/mrs/service/impl/HomeServiceImpl.java
@@ -2,6 +2,8 @@ package com.mrs.service.impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,16 +14,20 @@ import com.mrs.service.HomeService;
 @Service
 public class HomeServiceImpl implements HomeService{
 
+	Logger logger=LoggerFactory.getLogger(getClass());
+	
 	@Autowired
 	EmpRepo empRepo;
 	
 	@Override
 	public List<Emp> getAllEmployees(Emp emp) {
+		logger.info("find Employees by "+emp);
 		return (List<Emp>) empRepo.findAll();
 	}
 
 	@Override
 	public Emp getEmpById(Integer empid) {
+		logger.info("find Employees by empid : "+empid);
 		return empRepo.findOne(empid);
 	}
 
